40155d793447d336d10bf2285a7b7858ba08275b
heliorodri <heliorodri@github.com>
2021-07-05 16:08:10 -0300

    adding switchIfEmpty for findByid

--- src/main/java/com/heliorodri/springwebfluxpoc/controller/MovieController.java
+++ src/main/java/com/heliorodri/springwebfluxpoc/controller/MovieController.java
@@ -3,12 +3,16 @@ package com.heliorodri.springwebfluxpoc.controller;
 import com.heliorodri.springwebfluxpoc.domain.Movie;
 import com.heliorodri.springwebfluxpoc.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequestMapping("movies")
@@ -24,7 +28,8 @@ public class MovieController {
 
     @GetMapping("/{id}")
     public Mono<Movie> findById(@PathVariable int id){
-        return service.findById(id);
+        return service.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(NOT_FOUND, "Movie with id " + id + " not found")));
     }
 
 }
