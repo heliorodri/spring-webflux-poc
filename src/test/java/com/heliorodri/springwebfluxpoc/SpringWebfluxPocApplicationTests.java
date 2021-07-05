21c511fc4a55698f7501603376e45dd26b5a4418
heliorodri <heliorodri@github.com>
2021-07-05 14:26:11 -0300

    start project

---
+++ src/test/java/com/heliorodri/springwebfluxpoc/SpringWebfluxPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliorodri.springwebfluxpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
