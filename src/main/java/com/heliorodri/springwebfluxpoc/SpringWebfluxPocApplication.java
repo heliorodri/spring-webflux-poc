c468b76a24638b055929f2a3623238961c995a02
heliorodri <heliorodri@github.com>
2021-07-09 15:17:25 -0300

    refactoring BlockHound Install

--- src/main/java/com/heliorodri/springwebfluxpoc/SpringWebfluxPocApplication.java
+++ src/main/java/com/heliorodri/springwebfluxpoc/SpringWebfluxPocApplication.java
@@ -2,11 +2,15 @@ package com.heliorodri.springwebfluxpoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringWebfluxPocApplication {
 
     public static void main(String[] args) {
+        BlockHound.install(builder ->
+                builder.allowBlockingCallsInside("java.util.UUID", "randomUUID"));
+
         SpringApplication.run(SpringWebfluxPocApplication.class, args);
     }
 
